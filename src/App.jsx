26526a6bcd5f87e78a220757c5c422227849a746
Lucas Rossolini <lucas.rossolini@gmail.com>
2021-11-24 16:09:12 +0100

    Ajout de Framre motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
