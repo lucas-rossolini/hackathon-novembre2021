ac111a92048afbb34728449d406de6c363b69ba7
Lucas Rossolini <lucas.rossolini@gmail.com>
2021-11-24 15:40:11 +0100

    Initialisation du dossier de projet

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/Header/Header.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <h1>Le Delirium</h1>
+
+      {/* fin Header */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Header} />
+          <Route path="page/" element={Footer} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Contenu */}
+
+      {/* Fin Contenu */}
+
+      {/* Footer */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <p>header</p>
+    </div>
+  );
+}
+
+export default Header;
