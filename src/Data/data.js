6e8a86973292cfa4386097e3b48cf5101abd0f4f
NanakiMathieu <mathieu.nanaki@gmail.com>
2021-11-25 12:21:35 +0100

    add data file with 10 titles

---
+++ src/Data/data.js
@@ -0,0 +1,44 @@
+const titles = [
+  { name: "Adele", album: "30", title: "Easy on me", year: "2021" },
+  {
+    name: "Nolwenn Leroy",
+    album: "La Cavale",
+    title: "Brésil, Finistère",
+    year: "2021",
+  },
+  {
+    name: "Juliette Armanet",
+    album: "Brûler le feu",
+    title: "Le Dernier Jour du Disco",
+    year: "2021",
+  },
+  {
+    name: "Metroplex",
+    album: "Streetwise",
+    title: "The Day After",
+    year: "2016",
+  },
+  { name: "Stromae", album: "Santé", title: "Santé", year: "2021" },
+  {
+    name: "Queen",
+    album: "News of the World",
+    title: "We Will Rock You",
+    year: "1977",
+  },
+  { name: "IamDayLight", album: "Orion", title: "Blue", year: "2020" },
+  {
+    name: "Imagine Dragons",
+    album: "Mercury – Act 1",
+    title: "Wrecked",
+    year: "2021",
+  },
+  {
+    name: "Swedish House Mafia",
+    album: "Moth To A Flame",
+    title: "Moth To A Flame",
+    year: "2021",
+  },
+  { name: "The Kid Laroi", album: "Fuck Love", title: "Stay", year: "2020" },
+];
+
+export default titles;
