466575ad01e5f39d99f52b44e3c68abeae8f444a
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2021-11-25 13:20:39 +0100

    Ajout des playlis vibrante dans vibratePlayslist

--- src/Data/VibratePlaylist.js
+++ src/Data/VibratePlaylist.js
@@ -1,76 +1,3 @@
-const titles = [
-  {
-    name: "Adele",
-    album: "30",
-    title: "Easy on me",
-    year: "2021",
-    genre: "Pop",
-  },
-  {
-    name: "Nolwenn Leroy",
-    album: "La Cavale",
-    title: "Brésil, Finistère",
-    year: "2021",
-    genre: "Pop",
-  },
-  {
-    name: "Juliette Armanet",
-    album: "Brûler le feu",
-    title: "Le Dernier Jour du Disco",
-    year: "2021",
-    genre: "Pop",
-  },
-  {
-    name: "Metroplex",
-    album: "Streetwise",
-    title: "The Day After",
-    year: "2016",
-    genre: "Ambient",
-  },
-  {
-    name: "Stromae",
-    album: "Santé",
-    title: "Santé",
-    year: "2021",
-    genre: "Pop",
-  },
-  {
-    name: "Queen",
-    album: "News of the World",
-    title: "We Will Rock You",
-    year: "1977",
-    genre: "Rock",
-  },
-  {
-    name: "IamDayLight",
-    album: "Orion",
-    title: "Blue",
-    year: "2020",
-    genre: "Rock",
-  },
-  {
-    name: "Imagine Dragons",
-    album: "Mercury – Act 1",
-    title: "Wrecked",
-    year: "2021",
-    genre: "Alternative",
-  },
-  {
-    name: "Swedish House Mafia",
-    album: "Moth To A Flame",
-    title: "Moth To A Flame",
-    year: "2021",
-    genre: "Electro",
-  },
-  {
-    name: "The Kid Laroi",
-    album: "Fuck Love",
-    title: "Stay",
-    year: "2020",
-    genre: "Rap",
-  },
-];
-
 const playlist = {
   simpsons: [
     166, 320, 128, 257, 103, 249, 71, 145, 138, 366, 119, 273, 120, 248, 79,
@@ -139,5 +66,4 @@ if (isSupported) {
     vibrate(music);
   });
 }
-
-export default titles;
+export default playlist;
