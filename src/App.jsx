cfac0c6d1c9406341f7b08e09032918d3289a07d
Lucas Rossolini <lucas.rossolini@gmail.com>
2021-11-25 13:28:45 +0100

    Création du Player

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
+import Player from "./components/Player/Player.jsx";
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song1.jpg",
+      src: "./assets/music/on-n-on.mp3",
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3",
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      }
+      return currentSongIndex + 1;
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
       {/* Header */}
-      <h1>Le Delirium</h1>
 
       {/* fin Header */}
 
@@ -21,6 +61,7 @@ function App() {
       </BrowserRouter>
 
       {/* Contenu */}
+      <Player />
 
       {/* Fin Contenu */}
 
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useRef, useEffect } from "react";
+import Controls from "./Controls.jsx";
+import Details from "./Details.jsx";
+import song1 from "../../assets/images/song-1.jpg";
+
+import "./Player.css";
+
+function Player() {
+  const [songs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song1.jpg",
+      src: "./assets/music/on-n-on.mp3",
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3",
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+  ]);
+
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      }
+      return currentSongIndex + 1;
+    });
+  }, [currentSongIndex]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
+    }
+  });
+
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp += 1;
+
+        if (temp > songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp -= 1;
+
+        if (temp < 0) {
+          temp = songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
+  return (
+    <div className="c-player">
+      <audio src={songs[currentSongIndex].src} ref={audioEl}></audio>
+      <h4>Playing now</h4>
+      <Details song={songs[currentSongIndex]} />
+      <Controls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        SkipSong={SkipSong}
+      />
+      <p>
+        Next up:{" "}
+        <span>
+          {songs[nextSongIndex].title} by {songs[nextSongIndex].artist}
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Player;
